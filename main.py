88e7bd11c01f64651855d73d0a204b61ba7e2fd5
Th3Wiz4rd <stevierayga@gmail.com>
2025-07-01 17:48:48 -0400

    C: LLM Input adjustment

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import os
 from dotenv import load_dotenv
 from google import genai
-
+import sys
 
 def main():
+
+    if len(sys.argv) < 2:
+        print("Error: No prompt provided.")
+        print("Usage: python3 main.py \"your prompt goes gere\"")
+        sys.exit(1)
+
+    user_prompt = sys.argv[1]
+
+    print(f"Recieved prompt: \"{user_prompt}\"")
+          
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
